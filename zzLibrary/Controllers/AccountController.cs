a98a6488c991020f9a53a3d48e0c3fc20e5afa59
dongg <dongguho@gmail.com>
2017-06-16 19:28:54 +0800

    仿佛完成了注册登录【

--- zzLibrary/Controllers/AccountController.cs
+++ zzLibrary/Controllers/AccountController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 using zzLibrary.Models;
 
 namespace zzLibrary.Controllers
@@ -24,17 +29,90 @@ namespace zzLibrary.Controllers
             return "value";
         }
 
-        [HttpGet]
-        [ActionName("tryit")]
-        public Object tryit(string detail, int id)
+        [HttpPost]
+        [ActionName("login")]
+        public Object Login([FromBody]AccountMsg value)
         {
-            return new { detail = detail, id = id };
+            try
+            {
+                user _user = db.user.Find(value.Username);
+                if (_user.password == value.Password)
+                {
+                    string token = value.Username + value.Password;
+                    byte[] bytes = Encoding.UTF8.GetBytes(token);
+                    SHA256Managed hasher = new SHA256Managed();
+                    bytes = hasher.ComputeHash(bytes);
+                    string hashString = string.Empty;
+                    foreach (byte x in bytes)
+                    {
+                        hashString += String.Format("{0:x2}", x);
+                    }
+                    _user.token = hashString;
+                    db.SaveChanges();
+                    return new { token = hashString };
+                }
+                else return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var validationErrors in e.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}",
+                                                validationError.PropertyName,
+                                                validationError.ErrorMessage);
+                    }
+                }
+            }
+            catch (DataException)
+            {
+                return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+            return true;
+        }
+
+        [HttpPost]
+        [ActionName("signup")]
+        public Object Signup([FromBody]AccountMsg info)
+        {
+            user newUser = new user
+            {
+                user1 = info.Username,
+                password = info.Password,
+                isadmin = (info.Admincode == "rootAdmin")
+            };
+            try
+            {
+                db.user.Add(newUser);
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var validationErrors in e.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}",
+                                                validationError.PropertyName,
+                                                validationError.ErrorMessage);
+                    }
+                }
+            }
+            catch (DataException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                return new HttpResponseMessage(HttpStatusCode.NotImplemented);
+            }
+
+            return true;
         }
 
         // POST api/<controller>
         public void Post([FromBody]string value)
         {
-            
+
         }
 
         // PUT api/<controller>/5
@@ -46,5 +124,19 @@ namespace zzLibrary.Controllers
         public void Delete(int id)
         {
         }
+
+        public class AccountMsg
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public string Admincode { get; set; }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
     }
 }
