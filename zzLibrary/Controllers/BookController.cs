356d953260d2ba4bf5ce6599dbf4bc5f89b682ed
dongg <dongguho@gmail.com>
2017-06-22 16:32:39 +0800

    添加了一个请求转发api

--- zzLibrary/Controllers/BookController.cs
+++ zzLibrary/Controllers/BookController.cs
@@ -6,6 +6,10 @@ using System.Net.Http;
 using System.Web.Http;
 using ZZLibModel;
 using zzLibrary.DAOs;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
 
 namespace zzLibrary.Controllers
 {
@@ -15,16 +19,9 @@ namespace zzLibrary.Controllers
         /// 获取所有的书本列表
         /// </summary>
         /// <returns>全部书信息的列表</returns>
-        public Object Get()
+        public async Task<ICollection<BookMsg>> Get()
         {
-            return new BookDAO().db.book.Select(x => new
-            {
-                isbn = x.isbn,
-                title = x.title,
-                author = x.author,
-                edition = x.edition,
-                price = x.price
-            }).ToList();
+            return await new BookDAO().GetALLAsync();
         }
 
         /// <summary>
@@ -124,30 +121,49 @@ namespace zzLibrary.Controllers
         /// <returns></returns>
         [HttpGet]
         [ActionName("search")]
-        public Object Search(string title, int page)
+        public async Task<Object> Search(string title, int page)
         {
             var bookdao = new BookDAO();
-            var len = bookdao.FindAll(x => x.title.Contains(title)).Count();
+            var allResult = await bookdao.FindAllAsync(x => x.title.Contains(title));
+            var len = allResult.Count();
             var pageSize = 20; //每页长度
             var totalPage = (len + pageSize - 1) / pageSize;
             int startRow = (page - 1) * pageSize;
-            var books = bookdao.FindAll(x => x.title.Contains(title))
-                .OrderBy(x => x.title)
-                .Skip(startRow).Take(pageSize)
-                .Select(x => new
-                {
-                    title = x.title,
-                    author = x.author,
-                    edition = x.edition,
-                    isbn = x.isbn,
-                    price = x.price
-                })
-                .ToList();
+            var books = await bookdao.SearchAsync(title, page);
             return new
             {
                 total = totalPage,
                 books = books
             };
+        }
+
+        /// <summary>
+        /// 通过isbn获得某本书的信息（外网API转发）
+        /// </summary>
+        /// <param name="token">管理员token</param>
+        /// <param name="isbn">isbn号码</param>
+        /// <returns></returns>
+        [HttpGet]
+        [ActionName("info")]
+        public async Task<Object> info(string token, string isbn)
+        {
+            var usr = new UserDAO().GetByToken(token);
+            if (usr == null || !usr.isadmin)
+            {
+                var resp = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                resp.Content = new StringContent("Please log in as admin.");
+                throw new HttpResponseException(resp);
+            }
+
+            var request = WebRequest.Create("http://isbn.szmesoft.com/isbn/query?isbn=" + isbn) as WebRequest;
+            request.Method = "GET";
+            var re =  await request.GetResponseAsync();
+            using (var reader = new StreamReader(re.GetResponseStream()))
+            {
+                string result = reader.ReadToEnd();
+                var json = JsonConvert.DeserializeObject(result);
+                return json;
+            }
         }
     }
 }
--- zzLibrary/DAOs/BookDAO.cs
+++ zzLibrary/DAOs/BookDAO.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using ZZLibModel;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Threading.Tasks;
 
 namespace zzLibrary.DAOs
 {
     public class BookDAO : BaseDAO<book>
     {
-        
+        public async Task<ICollection<BookMsg>> SearchAsync(string title, int page)
+        {
+            var pageSize = 20;
+            int startRow = (page - 1) * pageSize;
+            return await db.book
+                .Where(x => x.title.Contains(title))
+                .OrderBy(x => x.title)
+                .Skip(startRow).Take(pageSize)
+                .Select(x => new BookMsg(x))
+                .ToListAsync();
+        }
+
+        public async Task<ICollection<BookMsg>> GetALLAsync()
+        {
+            return await db.book.Select(x => new BookMsg(x)).ToListAsync();
+        }
+    }
+
+    /// <summary>
+    /// 只有本体信息的book
+    /// </summary>
+    public class BookMsg
+    {
+        public BookMsg(book b)
+        {
+            isbn = b.isbn;
+            title = b.title;
+            author = b.author;
+            price = b.price;
+            edition = b.edition;
+        }
+        public string isbn { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string price { get; set; }
+        public string edition { get; set; }
     }
 }
